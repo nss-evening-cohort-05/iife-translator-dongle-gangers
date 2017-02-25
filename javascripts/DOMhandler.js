27512aa058586b8f73e9cd0cecff74c401f7b747
sharon smith <sharon.e.smith2@gmail.com>
2017-02-25 12:37:05 -0600

    working on DOMHandler.js

--- javascripts/DOMhandler.js
+++ javascripts/DOMhandler.js
@@ -1,1 +1,24 @@
 // Up to us to make these connections; no boilerplate
+var translator = ["Wookie", "French", "Korean", "Spanish"];
+var translationArray = [];
+var outputString = document.getElementById("translatedText");
+
+
+translator.addEventListener("change", function(event) {
+
+  // Get the selected language from the DOM
+  selectedLanguage = event.target.value;
+
+  // get the translation
+  // 
+  translationArray = DongleTranslator.translateTo(selectedLanguage);
+
+  // outputString.innerHTML = translationArray
+  // var translation = "";
+    	// for (var i in frenchWords) {
+     //  		translation += frenchWords + " ";
+     // 	 }
+     //    return translation;
+
+}
+
--- javascripts/french.js
+++ javascripts/french.js
@@ -2,12 +2,13 @@
     var frenchWords = {"my": "ma", "very": "très", "elegant": "élégant", "mother": "mère", "just": "à", "served": "sert", "us": "nous", "noodles": "des nouilles"};
     // word order :: ma mère très élégant à seulement nous sert des nouilles
 
-    oldDongleTranslator.translateToFrench = function(french){
-    	var translation = "";
-    	for (var i in frenchWords) {
-      		translation += frenchWords + " ";
-     	 }
-        return translation;
+    oldDongleTranslator.translateTo = function(french){
+    	// var translation = "";
+    	// for (var i in frenchWords) {
+     //  		translation += frenchWords + " ";
+     // 	 }
+     //    return translation;
+     return frenchWords;
     }
 
     return oldDongleTranslator;
