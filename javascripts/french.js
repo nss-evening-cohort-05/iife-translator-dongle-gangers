bdeca88eb56642341b2765fe04053a3e3d1f3d6b
sharon smith <sharon.e.smith2@gmail.com>
2017-02-25 12:02:10 -0600

    changes to french.js

--- javascripts/french.js
+++ javascripts/french.js
@@ -2,10 +2,10 @@
     var frenchWords = {"my": "ma", "very": "très", "elegant": "élégant", "mother": "mère", "just": "à", "served": "sert", "us": "nous", "noodles": "des nouilles"};
     // word order :: ma mère très élégant à seulement nous sert des nouilles
 
-    oldSandWhichMaker.translateTo = function(french){
+    oldDongleTranslator.translateToFrench = function(french){
     	var translation = "";
     	for (var i in frenchWords) {
-      		translation += frenchWords;
+      		translation += frenchWords + " ";
      	 }
         return translation;
     }
