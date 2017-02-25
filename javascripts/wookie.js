c814c0d8aa8b8e59025dfbcd8fc77bc3f21b4df2
C Wilkosz <theewilkyway@gmail.com>
2017-02-25 12:08:13 -0600

    tested

--- javascripts/wookie.js
+++ javascripts/wookie.js
@@ -23,22 +23,5 @@ var Translation = (function(oldDongleTranslator)){
 
 
 
-
-
-//wookie {"my": "", "very": "", "elegant": "", "mother": "", "just": "", "served": "", "us": "", "noodles": ""} -CW
-//  var SandwhichMaker = (function(oldSandWhichMaker){
-//     var meatz = {"cat": 25.50, "chicken": 0.50, "ham": 1.00}
-
-//     oldSandWhichMaker.getMeatz = function(){
-//         return meatz;
-//     };
-
-//     //input "cat"  ===> 25.50
-//     oldSandWhichMaker.getMeatPrice = function(meat){
-//         return meatz[meat]
-//     }
-
-//     return oldSandWhichMaker;
-// })(SandwhichMaker || {});
-"my very elegant mother just served us noodles"
-Wookie Translation: wrrhwrwwhw raaaaaahhgh aarrragghuuhw raaaaaahhgh huurh hnnnhrrhhh uughghhhgh huuguughghg
+// "my very elegant mother just served us noodles"
+// Wookie Translation: wrrhwrwwhw raaaaaahhgh aarrragghuuhw raaaaaahhgh huurh hnnnhrrhhh uughghhhgh huuguughghg
