3d6780686af954b70a16b19c49253c3c44518854
C Wilkosz <theewilkyway@gmail.com>
2017-02-25 12:04:11 -0600

    wookiespeek

--- javascripts/wookie.js
+++ javascripts/wookie.js
@@ -1,14 +1,44 @@
- var SandwhichMaker = (function(oldSandWhichMaker){
-    var meatz = {"cat": 25.50, "chicken": 0.50, "ham": 1.00}
+var Translation = (function(oldDongleTranslator)){
 
-    oldSandWhichMaker.getMeatz = function(){
-        return meatz;
-    };
+	var wookie = {"my": "wrrhwrwwhw", 
+				  "very": "raaaaaahhgh",
+				  "elegant": "aarrragghuuhw",
+				  "mother": "raaaaaahhgh",
+				  "just": "huurh",
+				  "served": "hnnnhrrhhh",
+				  "us": "uughghhhgh",
+				  "noodles": "huuguughghg"}
+}
+	oldDongleTranslator.translateToWookie =function(){
+		return wookie
+	}
 
-    //input "cat"  ===> 25.50
-    oldSandWhichMaker.getMeatPrice = function(meat){
-        return meatz[meat]
-    }
+	
 
-    return oldSandWhichMaker;
-})(SandwhichMaker || {});
+
+ return oldDongleTranslator;
+ })(dongleTranslator || {});
+
+
+
+
+
+
+
+//wookie {"my": "", "very": "", "elegant": "", "mother": "", "just": "", "served": "", "us": "", "noodles": ""} -CW
+//  var SandwhichMaker = (function(oldSandWhichMaker){
+//     var meatz = {"cat": 25.50, "chicken": 0.50, "ham": 1.00}
+
+//     oldSandWhichMaker.getMeatz = function(){
+//         return meatz;
+//     };
+
+//     //input "cat"  ===> 25.50
+//     oldSandWhichMaker.getMeatPrice = function(meat){
+//         return meatz[meat]
+//     }
+
+//     return oldSandWhichMaker;
+// })(SandwhichMaker || {});
+"my very elegant mother just served us noodles"
+Wookie Translation: wrrhwrwwhw raaaaaahhgh aarrragghuuhw raaaaaahhgh huurh hnnnhrrhhh uughghhhgh huuguughghg
